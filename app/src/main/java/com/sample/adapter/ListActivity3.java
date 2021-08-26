09df06cb0d3478cc923e99567dbc6ad118d31112
MacMini <macmini@MacMinideMac-mini.local>
2021-08-26 17:10:53 +0800

    0.0.6.5

--- app/src/main/java/com/sample/adapter/ListActivity3.java
+++ app/src/main/java/com/sample/adapter/ListActivity3.java
@@ -1,14 +1,36 @@
 package com.sample.adapter;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sample.adapter.adapters.MyAdapter2;
+
+import knight.rider.kitt.adapter.attr.DividerStyle;
+import knight.rider.kitt.adapter.impl.ItemDecorationImpl;
 
 public class ListActivity3 extends AppCompatActivity {
+
+    private MyAdapter2 adapter;
+
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list3);
+
+        RecyclerView rv = findViewById(R.id.rv2);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        gridLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        rv.setLayoutManager(gridLayoutManager);
+
+        rv.addItemDecoration(new ItemDecorationImpl(DividerStyle.GRID, Color.parseColor("#924569"), 20, 50, 100));
+
+        adapter = new MyAdapter2(this);
+        rv.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/sample/adapter/adapters/MyAdapter2.java
@@ -0,0 +1,52 @@
+package com.sample.adapter.adapters;
+
+
+import android.content.Context;
+import android.view.View;
+
+import com.sample.adapter.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+import knight.rider.kitt.adapter.SimpleListAdapter;
+import knight.rider.kitt.adapter.holder.RecyclerViewHolder;
+
+public class MyAdapter2 extends SimpleListAdapter<String> {
+
+    private static List<String> list;
+
+    private int index = 0;
+    private int index2 = 0;
+
+    View headers;
+
+    static {
+
+        list = Arrays.asList(
+                "我是第1条数据",
+                "我是第2条数据",
+                "我是第3条数据",
+                "我是第4条数据",
+                "我是第5条数据",
+                "我是第6条数据",
+                "我是第7条数据",
+                "我是第8条数据",
+                "我是第9条数据",
+                "我是第10条数据"
+        );
+    }
+
+
+    public MyAdapter2(Context context) {
+        super(context, R.layout.item_vertical, false, list);
+
+        headers = getInflater().inflate(R.layout.item_header_footer, null);
+    }
+
+
+    @Override
+    public void onBindViewHolders(RecyclerViewHolder holder, String s, int position) {
+        holder.getTextView(R.id.tv).setText(s);
+    }
+}
